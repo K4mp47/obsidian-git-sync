a4bd0139f69acce0487f81403f86815a91e6c0b0
K4mp47 <campagnoloalberto5@gmail.com>
2023-09-29 09:23:59 +0200

    vault backup: 2023-09-29 09:23:59

---
+++ Secondo Anno/Algoritmi & strutture dati/insertion_sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void insertion_sort(int a[], int n) {
+  for (int i = 1; i < n; i++) {
+    int key = a[i];
+    int j = i - 1;
+    while (j >= 0 && a[j] > key) 
+      a[j + 1] = a[j]; j -= 1;
+    a[j + 1] = key;
+  }
+}
+
+int main() {
+  int a[7];
+
+  for(int i=0; i < 7; i++) {
+    cin >> a[i];
+  }
+
+  insertion_sort(a, 7);
+  
+  for (int i = 0; i < 7; i++) {
+    cout << a[i] << " ";
+  }
+}
