f8cdbd5f33f26374639078a0584351d2ae817908
K4mp47 <campagnoloalberto5@gmail.com>
2023-11-16 10:08:23 +0100

    chore: OS_1_EXAM

---
+++ Secondo Anno/Algoritmi & strutture dati/code/esercitazione12/main.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <iomanip>
+
+// Campagnolo Alberto, 897569
+// Esercizio 1
+//
+// Complessità Theta(n), dovuta al fatto che l'algoritmo passa per ogni nodo una volta sola, il 
+// valore di ritorno viene salvato nella variabile reference t.
+//
+// Algoritmo: usa una funzione ausiliaria
+
+struct Node {
+  int key;
+  Node* left;
+  Node* right;
+};
+
+typedef Node* Pnode;
+
+bool inNonDec(Pnode u);
+
+class BinaryTree {
+public:
+  BinaryTree() : root(nullptr) {}
+  ~BinaryTree() {
+    destroy(root);
+  }
+  void insert(int key) {
+    root = insertRecursive(root, key);
+  };
+
+  void remove(int key) {
+    root = removeRecursive(root, key);
+  }
+  
+  void printInOrder() {
+    printInOrderRecursive(root,0);    
+  } 
+
+  Node* getRoot() {
+    return root;
+  }
+private:
+  Node* root;
+
+  Node* insertRecursive(Node* u, int key) {
+    if (u == nullptr) {
+      u = new Node;
+      u->key = key;
+      u->left = nullptr;
+      u->right = nullptr;
+    } else if (key < u->key) {
+      u->left = insertRecursive(u->left, key);
+    } else if (key > u->key) {
+      u->right = insertRecursive(u->right, key);
+    }
+    return u;
+  }
+
+  void printInOrderRecursive(Node* u, int indent) {
+    if (u != nullptr) {
+      printInOrderRecursive(u->right, indent+4);
+      std::cout << std::setw(indent) << " " << u->key << std::endl;
+      printInOrderRecursive(u->left, indent+4);
+    }
+  }
+
+  Node* removeRecursive(Node* u, int key) {
+    if (u == nullptr) {
+      return nullptr;
+    }
+    if (key < u->key) {
+      u->left = removeRecursive(u->left, key);
+    } else if (key > u->key) {
+      u->right = removeRecursive(u->right, key);
+    } else {
+      if (u->left == nullptr && u->right == nullptr) {
+        delete u;
+        u = nullptr;
+      } else if (u->left == nullptr) {
+        Node* tmp = u;
+        u = u->right;
+        delete tmp;
+      } else if (u->right == nullptr) {
+        Node* tmp = u;
+        u = u->left;
+        delete tmp;
+      } else {
+        Node* tmp = u->left;
+        while (tmp->right != nullptr) {
+          tmp = tmp->right;
+        }
+        u->key = tmp->key;
+        u->left = removeRecursive(u->left, tmp->key);
+      }  
+    }
+    return u;
+  }
+
+  void destroy(Node* u) {
+    if (u != nullptr) {
+      destroy(u->left);
+      destroy(u->right);
+      delete u;
+    }
+  }
+};
+
+int main (int argc, char *argv[]) {
+  BinaryTree tree;
+  tree.insert(30);
+  tree.insert(20);
+  tree.insert(22);
+  tree.insert(24);
+  tree.insert(23);
+  tree.insert(25);
+  tree.insert(29);
+  tree.insert(26);
+  tree.insert(27);
+  tree.insert(28);
+  tree.insert(15);
+  tree.insert(16);
+  std::cout << std::endl;
+  tree.printInOrder();
+  bool t = inNonDec(tree.getRoot());
+  std::cout << t << std::endl;
+  return 0;
+}
+
+bool inNonDec(Pnode u) {
+  if(u == nullptr) return true;
+  return inNonDec(u->left) && inNonDec(u->right) && (u->right)? u->key < u->right->key : true; 
+}
