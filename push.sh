58f94f68d14aa100166e51fcf6767855b21741ba
K4mp47 <campagnoloalberto5@gmail.com>
2024-02-12 18:31:39 +0100

    chore: update lun 12 feb 2024, 18:31:39, CET

--- push.sh
+++ push.sh
@@ -1,5 +1,7 @@
 #/usr/bin/bash
 
 git add .
-git commit -m "chore: OS_1_EXAM"
+# add the clock time of the commit to the message string
+# the date format is %Y-%m-%d %H:%M:%S
+git commit -m "chore: update $(date)"
 git push
